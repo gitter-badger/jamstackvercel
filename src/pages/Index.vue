250c24b9af05bcc80ae164ab186b905c62858454
DeKopon <dekopon.pr@gmail.com>
2020-07-19 21:36:39 +0900

    CSS->TailwindCSS5

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,22 +2,24 @@
     .index
       div(class="w-full h-20 pt-2 pb-0 px-1 table")
         div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-base font-bold")
-          img(Class="float-left mr-1" src="uploads/briefcase-solid.svg" width="24" height="24")
+          img(class="float-left mr-1" src="uploads/briefcase-solid.svg" width="24" height="24")
           p Portfolio.JAMstack
       .card1
-        img(Class="sm:float-left float-none sm:mr-3 mr-0" src="uploads/VilafrancaLemon.oxo.jpg" width="150" height="150")
+        img(class="sm:float-left float-none sm:mr-3 mr-0" src="uploads/VilafrancaLemon.oxo.jpg" width="150" height="150")
         .card1box
-          p(Class="text-xl font-black tracking-widest")
-            span(Class="name") Dekopon
-          p(Class="mt-3 text-base font-thin tracking-widest")
-            span(Class="note") portfolio.notion
+          p(class="text-xl font-black tracking-widest")
+            span(class="name") Dekopon
+          p(class="mt-3 text-base font-thin tracking-widest")
+            span(class="note") portfolio.notion
       div(class="w-full h-20 pt-2 pb-0 px-1 table")
-        div(Class="table-cell sm:px-0 px-1 align-middle tracking-widest text-base font-bold") retouch/ times
-      g-link(v-for='post in $page.posts.edges', :key='post.id', :to='post.node.path')
-        div(class="bg-orange-400 rounded-lg")
-          h2 {{ post.node.headline }}
-          div {{ post.node.description }}
-          div {{ post.node.tags }}
+        div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-base font-bold") retouch/ times
+      div(class="w-full")
+        g-link(v-for='post in $page.posts.edges', :key='post.id', :to='post.node.path')
+          .contbox
+            g-image(:src="post.node.img_import" width="100%" height="auto")
+            p.conttitle {{ post.node.headline }}
+            p.contdscrpt {{ post.node.description }}
+            //- p {{ post.node.tags }}
 </template>
 
 <style>
