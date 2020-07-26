3d6a7e673cbd6037ef1249df960daa77f0acd885
DeKopon <dekopon.pr@gmail.com>
2020-07-26 16:38:48 +0900

    fix

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   article
-    div(class="w-full h-20 pt-2 pb-0 px-1 table")
-        div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-sm font-thin")
-          p
-            <g-link to="/" class="link float-left mr-1">home/ </g-link>
-          p.hd {{ $page.post.headline }}
-    VueRemarkContent
+    .postbody
+      div(class="w-full h-20 pt-2 pb-0 px-1 table")
+          div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-sm font-thin")
+            p
+              <g-link to="/" class="link float-left mr-1">home/ </g-link>
+            p.hd {{ $page.post.headline }}
+      VueRemarkContent
 </template>
 
 <style>
