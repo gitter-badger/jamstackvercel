c346dd390061e6899e57b4558b6ff4839f867b14
DeKopon <dekopon.pr@gmail.com>
2020-08-23 17:33:37 +0900

    GridLayout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
     .index
-      div(class="w-full h-20 pt-2 pb-0 px-1 table")
-        div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-base font-bold")
-          p(class="float-left mr-1")
-            img(src="uploads/briefcase-solid.svg" width="24" height="24")
-          p.sitetitle Portfolio.JAMstack
+      div(class="w-56 h-20 pt-2 pb-0 px-1 grid grid-flow-col sm:px-0 px-1 tracking-widest text-base font-bold text-left")
+        p(class="mr-0")
+          img(src="uploads/briefcase-solid.svg" width="24" height="24")
+        p.sitetitle Portfolio.JAMstack
       .card1
         img(src="uploads/VilafrancaLemon.oxo.jpg" width="150" height="150")
         .card1box
@@ -12,8 +11,7 @@
             span(class="name") Dekopon
           p(class="mt-3 text-base font-thin tracking-widest")
             span(class="note") Portfolio
-      div(class="w-full h-20 pt-2 pb-0 px-1 table clear-left")
-        div(class="table-cell sm:px-0 px-1 align-middle tracking-widest text-base font-bold") retouch/ times
+      div(class="w-full my-6 pt-2 pb-0 px-1 sm:px-0 px-1 tracking-widest text-base font-bold") retouch/ times
       .contcardbox
         g-link(v-for='post in $page.posts.edges', :key='post.id', :to='post.node.path')
           .contbox
